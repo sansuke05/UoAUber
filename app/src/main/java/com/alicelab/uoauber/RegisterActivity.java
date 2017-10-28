67f85352e5d829d8ef1424d6353d4c41919c31f5
Takanori Watanabe <takanori5064@gmail.com>
2017-10-29 02:19:13 +0900

    位置情報取得機能作成中

--- app/src/main/java/com/alicelab/uoauber/RegisterActivity.java
+++ app/src/main/java/com/alicelab/uoauber/RegisterActivity.java
@@ -1,22 +1,31 @@
 package com.alicelab.uoauber;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity implements LocationListener {
 
     private LocationManager mLocationManager;
+    private String latitude = "";
+    private String longitude = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +33,28 @@ public class RegisterActivity extends AppCompatActivity implements LocationListe
         setContentView(R.layout.activity_register);
 
         Button button = (Button)findViewById(R.id.register);
+        
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+        }
+        else{
+            locationStart();
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void locationStart(){
+        Log.d("debug","locationStart()");
 
         mLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        boolean gpsFlg = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        final boolean gpsFlg = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
         if (!gpsFlg) {
             // GPSを設定するように促す
@@ -37,14 +65,68 @@ public class RegisterActivity extends AppCompatActivity implements LocationListe
             Log.d("debug", "gpsEnabled");
         }
 
-        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 50, RegisterActivity.this);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            Log.d("debug", "checkSelfPermission false");
+            return;
+        }
 
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location){
+        latitude = Double.toString(location.getLatitude()); // 緯度
+        longitude = Double.toString(location.getLongitude()); // 軽度
+
+        mLocationManager.removeUpdates(this);
+        Log.d("RegisterActivity", "緯度：" + latitude);
+        Log.d("RegisterActivity", "経度：" + longitude);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1000) {
+            // 使用が許可された
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("debug","checkSelfPermission true");
+
+                locationStart();
+                return;
+
+            } else {
+                // それでも拒否された時の対応
+                Toast toast = Toast.makeText(this, "これ以上なにもできません", Toast.LENGTH_SHORT);
+                toast.show();
             }
-        });
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        switch (status) {
+            case LocationProvider.AVAILABLE:
+                Log.d("debug", "LocationProvider.AVAILABLE");
+                break;
+            case LocationProvider.OUT_OF_SERVICE:
+                Log.d("debug", "LocationProvider.OUT_OF_SERVICE");
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE");
+                break;
+        }
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 
 }
