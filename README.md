5a9bd4d44a5114c3ab671fa8c2dc631792c86c77
Takanori Watanabe <takanori5064@gmail.com>
2017-10-29 01:01:53 +0900

    add README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+## 進捗(2017/10/29 0:45)
+
+get_want_rideからドライバーのリストを取得、表示する MainActivity のリストはhttpでデータ取得、表示できるとこまでは完成（細かい修正が必要）
+
+
+registerにpostする機能は完成済み(PostAPIConnection.java)
+
+
+registerの画面とGPSから現在位置情報を取得する機能を作成中(RegisterActivity.java)
+
+
+## 作成予定
+
+起動→乗りたいor新規登録選択画面→新規登録画面（作成中）
+　　　　↓
+　　ドライバー情報リスト画面（作成済み）
+　　
--- app/src/main/java/com/alicelab/uoauber/RegisterActivity.java
+++ app/src/main/java/com/alicelab/uoauber/RegisterActivity.java
@@ -1,27 +1,48 @@
 package com.alicelab.uoauber;
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements LocationListener {
+
+    private LocationManager mLocationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button button = (Button)findViewById(R.id.register);
+
+        mLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        boolean gpsFlg = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (!gpsFlg) {
+            // GPSを設定するように促す
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+            Log.d("debug", "not gpsEnable, startActivity");
+        } else {
+            Log.d("debug", "gpsEnabled");
+        }
+
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 50, RegisterActivity.this);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
     }
